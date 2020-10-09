2310d927d7cf6145be9f4feba832ecd5eeb68de0
willh99 <willh99@gwmail.gwu.edu>
2020-10-08 23:23:24 -0400

    updating and working well enough

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
 import json
 import os
+from re import escape
 import scrapy
-
-page_count = 0
 
 class BriefingsSpider(scrapy.Spider):
     name = "briefings_spinder"
@@ -17,16 +16,13 @@ class BriefingsSpider(scrapy.Spider):
                     self.parse_briefing
                 )
 
-
     def parse_briefing(self, response):
-        global page_count
-
         url = response.request.url
         header = response.xpath('//h1/center/text()').extract_first()
         title = response.xpath('//h2/center/text()').extract_first()
-        info =  response.xpath('/html/body/nextid/center/text()').getall()
         page_text = ''
 
+        # Get all the speech text on a page
         for block in response.xpath('//p'):
             text_header = block.xpath('b/text()').extract()
             text_block = block.xpath('text()').extract()
@@ -36,33 +32,39 @@ class BriefingsSpider(scrapy.Spider):
 
             if text_block is not None:
                 text_block = text_header + text_block
-
-            #''.join(text_block).replace('\\\\n', '\\n')
-
             if text_block is not None and text_block:
-                page_text += f'{text_block}'
-
+                page_text += text_block
 
         # Some pages are formatted differently (and all of them look terrible)
         # so try to handle those different cases here
         if header is None:
+            header = response.xpath('//p[@align = "CENTER"][2]/text()').extract()
+            title  = response.xpath('//p[@align = "CENTER"][3]/text()').extract()
+            title += response.xpath('//p[@align = "CENTER"][4]/text()').extract()
+            title += response.xpath('//p[@align = "CENTER"][5]/text()').extract()
+            title += response.xpath('//p[@align = "CENTER"][6]/text()').extract()
+
+        if header is None:
             return
-            header = response.xpath('//center/font/text()').extract_first()
-        if title is None:
-            title = response.xpath('//center/font/br/br/following::text()').extract_first()
+
+        page_text = page_text.strip().replace("\n", "\\n").replace('"', '\\"')
 
         page_data = {
             'url': url,
             'header': header,
             'title': title,
-            'text': page_text.strip()
+            'text': page_text
         }
-        # Write out the scraped data to a file
-        if not os.path.isdir('data'):
-            os.mkdir('data')
-        with open(f'data/{page_count}.json', 'w') as f:
-            json.dump(page_data, f)
 
-        page_count += 1
+        # Write out the scraped data to a file
+        if os.path.isfile('reports.json'):
+            with open('reports.json', 'r') as f:
+                report_json = json.load(f)
+        else:
+            report_json = {}
+            
+        report_json[url.split('/')[-1]] = page_data  
+        with open('reports.json', 'w') as f:
+            json.dump(report_json, f, indent=2)
 
         return page_data
